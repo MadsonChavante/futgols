0626c4e8561d6f8d89283409288bec236f7d4de7
MadsonChavante <ml.chavante@gmail.com>
2019-06-13 17:21:30 -0300

    escudos e rodadas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AppService } from '../app.service';
 import { StoregeProvider } from 'src/storege/storege';
 import { LocalUser } from 'src/models/local_user';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -11,28 +12,60 @@ import { LocalUser } from 'src/models/local_user';
 export class Tab2Page {
 
   private grupos: any;
+  currentMatchday;
 
   constructor(
-    private appService:AppService,
-    private storegeService:StoregeProvider
-  ) {}
+    private appService: AppService,
+    private storegeService: StoregeProvider,
+    private platform: Platform,
+  ) { }
 
   ngOnInit() {
-    var localUser:LocalUser = this.storegeService.currentLocalUserValue;
-    this.appService.getMatchs(localUser.currentMatchday).subscribe( res => {
-      
-      this.grupos = this.groupBy(res['matches'],"utcDate");
-      this.grupos = Object.entries(this.grupos);
-      console.log(this.grupos);
-      
+    
+    var localUser: LocalUser = this.storegeService.currentLocalUserValue;
+    this.currentMatchday = localUser.currentMatchday;
+    this.getMatchs(true);
+    
+  }
 
+  getMatchs(scroll = false){
+    this.appService.getMatchs(this.currentMatchday).subscribe(res => {
+
+      this.grupos = this.groupBy(res['matches'], "utcDate");
+      this.grupos = Object.entries(this.grupos);
       
+      if(scroll){
+        this.scroll();
+      }
+
     })
   }
 
+  scroll() {
+    
+    this.platform.ready().then((readySource) => {
+      
+      var w = this.platform.width();
+      var x = Math.trunc(w / 90);
+      var y = this.currentMatchday - x;
+      if (y > 0) {
+        var scroll = y * 90;
+        var teste = document.getElementById("segment");
+        teste.scrollTo(scroll + 90, 0);
+      }
+
+    });
+  }
+
+  changeSegment(el) {
+    this.currentMatchday = +el.detail.value;
+    this.grupos = null;
+    this.getMatchs();
+  }
+
   groupBy(arr, property) {
-    var isso =  this;
-    return arr.reduce(function(memo, x) {
+    var isso = this;
+    return arr.reduce(function (memo, x) {
       var p = isso.convertDate(x[property]);
       if (!memo[p]) { memo[p] = []; }
       memo[p].push(x);
@@ -41,12 +74,12 @@ export class Tab2Page {
   }
 
 
-  convertDate(date){
-    var today  = new Date(date);    
-    return today.toLocaleDateString("pt-BR",{
+  convertDate(date) {
+    var today = new Date(date);
+    return today.toLocaleDateString("pt-BR", {
       hour: '2-digit',
-      minute:'2-digit'
-    }).replace("/2019 "," - ");
+      minute: '2-digit'
+    }).replace("/2019 ", " - ");
   }
 
 }
